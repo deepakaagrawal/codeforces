c53542224f9782197930560559d5209aeaeddf47
deepakaagrawal <deepak.aagrawal@gmail.com>
2024-08-30 13:54:48 +0530

    4A

--- Watermelon.java
+++ Watermelon.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
 
 public class Watermelon {
-public static void main(String[] args) {
-	long a = -1;
-	Long b= null;
-	a = b;
-	System.out.println(a);
-	
-}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = 0;
+		num = scan.nextInt();
+		if (num < 4) {
+			System.out.println("NO");
+			return;
+		}
+		if (num % 2 == 0)
+			System.out.println("YES");
+		else
+			System.out.println("NO");
+
+	}
 }
